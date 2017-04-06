5129b963398461f72f0ce78f6c8e7048ccfc27b2
colm.cavanagh <colm.cavanagh@digital.hmrc.gov.uk>
2017-04-06 12:46:33 +0100

    added in frist draft of the declaration page with hard coded values changed routes to declaration page added in new folders Added UserDetails case class Brought in cats

--- app/uk/gov/hmrc/helptosavefrontend/models/ContactPreference.scala
+++ app/uk/gov/hmrc/helptosavefrontend/models/ContactPreference.scala
@@ -1,4 +1,4 @@
-@*
+/*
  * Copyright 2017 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,16 +12,21 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *@
+ */
 
-@(pageTitle: String, heading: String, message: String)(implicit request: Request[_], messages: Messages)
+package uk.gov.hmrc.helptosavefrontend.models
 
-@contentHeader = {
-  <h1>@heading</h1>
+import cats.Show
+
+sealed trait ContactPreference
+
+object ContactPreference {
+  case object Email extends ContactPreference
+  case object SMS extends ContactPreference
+
+  implicit val contactPreferenceShow: Show[ContactPreference] = Show.show(_ match {
+    case Email => "Email"
+    case SMS => "SMS"
+  })
 }
 
-@mainContent = {
-  <p>@message</p>
-}
-
-@govuk_wrapper(appConfig = uk.gov.hmrc.helptosavefrontend.FrontendAppConfig, title = pageTitle, contentHeader = Some(contentHeader), mainContent = mainContent)
---
+++ test/uk/gov/hmrc/helptosavefrontend/HelpToSaveSpec.scala
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.helptosavefrontend.controllers
+
+import play.api.http.Status
+import play.api.test.FakeRequest
+import play.api.test.Helpers._
+import play.api.http._
+import play.api.test.FakeRequest
+import play.api.test.Helpers._
+import uk.gov.hmrc.play.test.{WithFakeApplication, UnitSpec}
+
+import cats.syntax.show._
+
+class HelpToSaveSpec extends UnitSpec with WithFakeApplication{
+
+  val fakeRequest = FakeRequest("GET", "/")
+
+
+  "GET /" should {
+    "return 200" in {
+      val result = HelpToSave.helpToSave(fakeRequest)
+      status(result) shouldBe Status.OK
+    }
+
+    "return HTML" in {
+      val result = HelpToSave.helpToSave(fakeRequest)
+      contentType(result) shouldBe Some("text/html")
+      charset(result) shouldBe Some("utf-8")
+    }
+
+    "display the user details" in {
+      // TODO: pass in user details in the request when this is implemented - we're just
+      //       using the hard-coded user at the moment
+      val html = contentAsString(HelpToSave.helpToSave(fakeRequest))
+
+      html should include(HelpToSave.user.name)
+      html should include(HelpToSave.user.dateOfBirth.show)
+      html should include(HelpToSave.user.email)
+      html should include(HelpToSave.user.NINO)
+      html should include(HelpToSave.user.phoneNumber)
+      html should include(HelpToSave.user.address.mkString(","))
+      html should include(HelpToSave.user.contactPreference.show)
+    }
+  }
+
+
+}
