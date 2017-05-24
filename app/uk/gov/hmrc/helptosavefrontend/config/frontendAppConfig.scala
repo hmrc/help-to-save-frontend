1ff0a360e95e388823b5a85190d6dfb46966f862
Jacqueline <jacqueline.hubbard@digital.hmrc.gov.uk>
2017-05-24 16:41:52 +0100

    HTS-164: Implemented SSL Play WS via config

--- app/uk/gov/hmrc/helptosavefrontend/config/frontendAppConfig.scala
+++ app/uk/gov/hmrc/helptosavefrontend/config/frontendAppConfig.scala
@@ -53,4 +53,5 @@ object FrontendAppConfig extends AppConfig with ServicesConfig {
   override lazy val analyticsHost = loadConfig(s"google-analytics.host")
   override lazy val reportAProblemPartialUrl = s"$contactHost/contact/problem_reports_ajax?service=$contactFormServiceIdentifier"
   override lazy val reportAProblemNonJSUrl = s"$contactHost/contact/problem_reports_nonjs?service=$contactFormServiceIdentifier"
+
 }
