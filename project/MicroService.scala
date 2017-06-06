e746dc3f94942be30cc803acd5562250b751390a
Andy <andrew.chung@digital.hmrc.gov.uk>
2017-06-06 11:09:59 +0100

    Added tags as a command line option in selenium tests

--- project/MicroService.scala
+++ project/MicroService.scala
@@ -1,9 +1,8 @@
 import sbt.Keys._
-import sbt.Tests.{SubProcess, Group}
+import sbt.Tests.{Group, SubProcess}
 import sbt._
 import play.routes.compiler.StaticRoutesGenerator
 import uk.gov.hmrc.sbtdistributables.SbtDistributablesPlugin._
-
 
 trait MicroService {
 
@@ -23,14 +22,11 @@ trait MicroService {
   lazy val plugins: Seq[Plugins] = Seq.empty
   lazy val playSettings: Seq[Setting[_]] = Seq.empty
 
-  def seleniumTestFilter(name: String): Boolean = name.endsWith("SeleniumSystemTest") && !name.contains("WIP")
+  def seleniumTestFilter(name: String): Boolean = name.endsWith("SeleniumSystemTest")
 
-  def seleniumWIPTestFilter(name: String): Boolean = name.endsWith("SeleniumSystemTest") && name.contains("WIP")
-
-  def unitTestFilter(name: String): Boolean = !seleniumTestFilter(name) && !seleniumWIPTestFilter(name)
+  def unitTestFilter(name: String): Boolean = !seleniumTestFilter(name)
 
   lazy val SeleniumTest = config("selenium") extend (Test)
-  lazy val SeleniumTestWIP = config("selenium-wip") extend (Test)
 
 
   lazy val microservice = Project(appName, file("."))
@@ -58,14 +54,12 @@ trait MicroService {
       Resolver.jcenterRepo
     ))
     .configs(SeleniumTest)
-    .configs(SeleniumTestWIP)
     .settings(
       inConfig(SeleniumTest)(Defaults.testTasks),
-      inConfig(SeleniumTestWIP)(Defaults.testTasks),
+      Keys.fork in SeleniumTest := true,
       unmanagedSourceDirectories in Test += baseDirectory.value / "selenium-system-test",
       testOptions in Test := Seq(Tests.Filter(unitTestFilter)),
-      testOptions in SeleniumTest := Seq(Tests.Filter(seleniumTestFilter)),
-      testOptions in SeleniumTestWIP := Seq(Tests.Filter(seleniumWIPTestFilter))
+      testOptions in SeleniumTest := Seq(Tests.Filter(seleniumTestFilter))
     )
 }
 
