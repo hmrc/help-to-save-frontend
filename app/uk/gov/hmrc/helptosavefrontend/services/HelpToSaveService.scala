970a33de8071784484b09f81c0bd889898e9633d
Suresh Mandalapu <suresh.mandalapu@digital.hmrc.gov.uk>
2017-06-19 15:34:17 +0100

    HTS-187: Fix the regex for coverageExcludedPackages(WIP) (#92)

--- app/uk/gov/hmrc/helptosavefrontend/services/HelpToSaveService.scala
+++ app/uk/gov/hmrc/helptosavefrontend/services/HelpToSaveService.scala
@@ -36,7 +36,6 @@ class HelpToSaveService @Inject()(helpToSaveConnector: HelpToSaveConnector, nSIC
   def checkEligibility(nino: String, userDetailsURI: String)(implicit hc: HeaderCarrier): Result[EligibilityResult] =
     helpToSaveConnector.getEligibilityStatus(nino, userDetailsURI)
 
-
   def createAccount(userInfo: NSIUserInfo)(implicit hc: HeaderCarrier, ec: ExecutionContext): EitherT[Future,SubmissionFailure,SubmissionSuccess] =
     EitherT(nSIConnector.createAccount(userInfo).map[Either[SubmissionFailure,SubmissionSuccess]] {
       case success: SubmissionSuccess =>
--- project/MicroService.scala
+++ project/MicroService.scala
@@ -26,8 +26,8 @@ trait MicroService {
     import scoverage.ScoverageKeys
     Seq(
       // Semicolon-separated list of regexs matching classes to exclude
-      ScoverageKeys.coverageExcludedPackages := "<empty>;Reverse.*;model.*;config.*;.*(AuthService|BuildInfo|Routes).*",
-      ScoverageKeys.coverageMinimum := 60,
+      ScoverageKeys.coverageExcludedPackages := "<empty>;Reverse.*;.*(config|views.*|models.*);.*(AuthService|BuildInfo|Routes).*",
+      ScoverageKeys.coverageMinimum := 84,
       ScoverageKeys.coverageFailOnMinimum := true,
       ScoverageKeys.coverageHighlighting := true,
       parallelExecution in Test := false
