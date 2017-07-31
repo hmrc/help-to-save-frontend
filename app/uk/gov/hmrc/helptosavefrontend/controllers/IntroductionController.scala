11c593a265c6b40bc91e18d85c97a44cef8d55a6
James A Birrell Gray <james@jamesbirrellgray.com>
2017-07-31 12:05:50 +0100

    HTS-1 - This is a merge of all the FE tickets under this epic (#128)

---
+++ app/uk/gov/hmrc/helptosavefrontend/controllers/IntroductionController.scala
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.helptosavefrontend.controllers
+
+import javax.inject.Singleton
+
+import com.google.inject.Inject
+import play.api.Application
+import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.mvc._
+import uk.gov.hmrc.helptosavefrontend.views
+
+import scala.concurrent.Future
+
+@Singleton
+class IntroductionController @Inject()(implicit app: Application, val messagesApi: MessagesApi) extends HelpToSaveAuth(app) with I18nSupport {
+
+  def getAboutHelpToSave: Action[AnyContent] = unprotected {
+    implicit request ⇒
+      implicit htsContext ⇒
+        Future.successful(Ok(views.html.introduction.about_help_to_save()))
+  }
+
+  def getEligibility: Action[AnyContent] = unprotected {
+    implicit request ⇒
+      implicit htsContext ⇒
+        Future.successful(Ok(views.html.introduction.eligibility()))
+  }
+
+  def getHowTheAccountWorks: Action[AnyContent] = unprotected {
+    implicit request ⇒
+      implicit htsContext ⇒
+        Future.successful(Ok(views.html.introduction.how_the_account_works()))
+  }
+
+  def getHowWeCalculateBonuses: Action[AnyContent] = unprotected {
+    implicit request ⇒
+      implicit htsContext ⇒
+        Future.successful(Ok(views.html.introduction.how_we_calculate_bonuses()))
+  }
+
+  def getApply: Action[AnyContent] = unprotected {
+    implicit request ⇒
+      implicit htsContext ⇒
+        Future.successful(Ok(views.html.introduction.apply()))
+  }
+
+}
