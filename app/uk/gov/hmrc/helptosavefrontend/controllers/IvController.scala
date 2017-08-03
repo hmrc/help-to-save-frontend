ab179fbe487235b9a60ec4fc951fbd2d398ba02d
Suresh Mandalapu <suresh.mandalapu@digital.hmrc.gov.uk>
2017-08-03 16:01:11 +0100

    HTS-392 : Fix redirection Url (#145)

--- app/uk/gov/hmrc/helptosavefrontend/controllers/IvController.scala
+++ app/uk/gov/hmrc/helptosavefrontend/controllers/IvController.scala
@@ -50,7 +50,7 @@ class IvController @Inject()(val sessionCacheConnector: SessionCacheConnector,
         case Some(id) ⇒
           ivConnector.getJourneyStatus(JourneyId(id)).map {
             case Some(Success) ⇒
-              Ok(iv_success(FrontendAppConfig.HtsConfirmDetailsUrl))
+              Ok(iv_success(FrontendAppConfig.CheckEligibilityUrl))
 
             case Some(Incomplete) ⇒
               //The journey has not been completed yet.
