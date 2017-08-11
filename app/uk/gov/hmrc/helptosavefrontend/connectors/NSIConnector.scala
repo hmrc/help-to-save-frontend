d6f4fec2fc057a6c7b7326e7453effeb91c55a16
Andy Dwelly <andydwelly@gmail.com>
2017-08-11 15:33:45 +0100

    Renamed audit source to meet product name.

--- app/uk/gov/hmrc/helptosavefrontend/connectors/NSIConnector.scala
+++ app/uk/gov/hmrc/helptosavefrontend/connectors/NSIConnector.scala
@@ -28,6 +28,7 @@ import uk.gov.hmrc.helptosavefrontend.connectors.NSIConnector.{SubmissionFailure
 import uk.gov.hmrc.helptosavefrontend.models.{ApplicationSubmittedEvent, NSIUserInfo}
 import uk.gov.hmrc.helptosavefrontend.util.{HTSAuditor, Logging}
 import uk.gov.hmrc.helptosavefrontend.util.HttpResponseOps._
+import uk.gov.hmrc.play.config.AppName
 import uk.gov.hmrc.play.http._
 
 import scala.concurrent.{ExecutionContext, Future}
@@ -50,7 +51,7 @@ object NSIConnector {
 }
 
 @Singleton
-class NSIConnectorImpl @Inject()(conf: Configuration, auditor: HTSAuditor) extends NSIConnector with Logging {
+class NSIConnectorImpl @Inject()(conf: Configuration, auditor: HTSAuditor) extends NSIConnector with Logging with AppName {
 
   val httpProxy = new WSHttpProxy
 
@@ -67,7 +68,7 @@ class NSIConnectorImpl @Inject()(conf: Configuration, auditor: HTSAuditor) exten
       .map { response ⇒
         response.status match {
           case Status.CREATED ⇒
-            auditor.sendEvent(new ApplicationSubmittedEvent(userInfo))
+            auditor.sendEvent(new ApplicationSubmittedEvent(appName, userInfo))
             logger.info(s"Received 201 from NSI, successfully created account for ${userInfo.nino}")
             SubmissionSuccess()
 
