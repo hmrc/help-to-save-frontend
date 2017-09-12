8be73a6694f4208b4d9b449e01405cfed4d0daec
suresh mandalapu <suresh.mandalapu@digital.hmrc.gov.uk>
2017-09-12 10:33:33 +0100

    Use latest versions of libraries

--- project/FrontendBuild.scala
+++ project/FrontendBuild.scala
@@ -10,20 +10,20 @@ object FrontendBuild extends Build with MicroService {
 
   val compile = Seq(
     ws,
-    "uk.gov.hmrc" %% "frontend-bootstrap" % "7.22.0",
-    "uk.gov.hmrc" %% "play-partials" % "5.3.0",
+    "uk.gov.hmrc" %% "frontend-bootstrap" % "8.6.0",
+    "uk.gov.hmrc" %% "play-partials" % "6.0.0",
     "uk.gov.hmrc" %% "play-config" % "4.3.0",
     "uk.gov.hmrc" %% "logback-json-logger" % "3.1.0",
-    "uk.gov.hmrc" %% "govuk-template" % "5.2.0",
+    "uk.gov.hmrc" %% "govuk-template" % "5.8.0",
     "uk.gov.hmrc" %% "play-health" % "2.1.0",
-    "uk.gov.hmrc" %% "http-caching-client" % "6.2.0",
-    "uk.gov.hmrc" %% "play-ui" % "7.2.1",
+    "uk.gov.hmrc" %% "http-caching-client" % "7.0.0",
+    "uk.gov.hmrc" %% "play-ui" % "7.5.0",
     "org.typelevel" %% "cats" % "0.9.0",
     "uk.gov.hmrc" %% "play-auth" % "2.2.0",
     "uk.gov.hmrc" %% "domain" % "4.1.0",
     "com.github.kxbmap" %% "configs" % "0.4.4",
     "com.eclipsesource" %% "play-json-schema-validator" % "0.8.9",
-    "uk.gov.hmrc" %% "play-reactivemongo" % "5.2.0"
+    "uk.gov.hmrc" %% "play-reactivemongo" % "6.0.0"
   )
 
   def test(scope: String = "test") = Seq(
