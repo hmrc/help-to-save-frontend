df55b25125ae72abb41b341f6ed472f1ba0baaad
Jacqueline <jacqueline.hubbard@digital.hmrc.gov.uk>
2017-09-26 14:44:18 +0100

    HTS-222 Removed unused code

--- run_zap_tests.sh
+++ run_zap_tests.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 echo "Running zap browser tests"
-#sbt -Dbrowser=zap-chrome -Denvironment=Local 'test-only hts.suites.RunZapTests'
 ./run_selenium_system_test.sh local zap-chrome @zap
 
 sleep 5
 
-echo "Running Zap Tests"
+echo "Running ZapRunner"
 sbt "testOnly ZapRunner"
