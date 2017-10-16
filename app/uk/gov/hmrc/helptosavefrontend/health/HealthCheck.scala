53a531996230bd36767b06f11716601ece2fbf98
Andy <andrew.chung@digital.hmrc.gov.uk>
2017-10-16 11:50:39 +0100

    formatting

--- app/uk/gov/hmrc/helptosavefrontend/health/HealthCheck.scala
+++ app/uk/gov/hmrc/helptosavefrontend/health/HealthCheck.scala
@@ -33,27 +33,27 @@ import scala.concurrent.duration._
 import scala.util.control.NonFatal
 
 /**
-  * An actor which handles the results of a generic health test. The tests that will be run are
-  * defined by `runnerProps`. The [[HealthCheck]] actor will repeatedly create a child using these props
-  * at a configured time interval. These children will have to respond to a
-  * [[uk.gov.hmrc.helptosavefrontend.health.HealthCheck.PerformTest]] message and respond with a [[HealthCheckResult]].
-  * The appropriate interface of the children is handily provided by the [[HealthCheckRunner]] trait. After a
-  * configured number of successful tests the [[HealthCheck]] actor will switch tests by using the next [[Props]] in
-  * `runnerProps`. When the last [[Props]] in `runnerProps` is used, the next switch will result in the first
-  * [[Props]] being used again and the [[Props]] are iterated through again in a cyclical fashion.
-  *
-  * The [[HealthCheck]] actor will handle [[HealthCheckResult]]s by keeping a record of the number of failures using
-  * the given [[Metrics]] and triggering pager duty alerts when a configured number of consecutive failures
-  * has occurred.
-  *
-  * @param name The name of the test
-  * @param config The config variables will be looked for under the namespace `health-check.{name}` where `name` is
-  *               the name of the test
-  * @param scheduler      The scheduler used to schedule the health tests
-  * @param metrics        The metrics to be updated
-  * @param pagerDutyAlert A function which will alert pager duty
-  * @param runnerProps    The props of the tests to be run
-  */
+ * An actor which handles the results of a generic health test. The tests that will be run are
+ * defined by `runnerProps`. The [[HealthCheck]] actor will repeatedly create a child using these props
+ * at a configured time interval. These children will have to respond to a
+ * [[uk.gov.hmrc.helptosavefrontend.health.HealthCheck.PerformTest]] message and respond with a [[HealthCheckResult]].
+ * The appropriate interface of the children is handily provided by the [[HealthCheckRunner]] trait. After a
+ * configured number of successful tests the [[HealthCheck]] actor will switch tests by using the next [[Props]] in
+ * `runnerProps`. When the last [[Props]] in `runnerProps` is used, the next switch will result in the first
+ * [[Props]] being used again and the [[Props]] are iterated through again in a cyclical fashion.
+ *
+ * The [[HealthCheck]] actor will handle [[HealthCheckResult]]s by keeping a record of the number of failures using
+ * the given [[Metrics]] and triggering pager duty alerts when a configured number of consecutive failures
+ * has occurred.
+ *
+ * @param name The name of the test
+ * @param config The config variables will be looked for under the namespace `health-check.{name}` where `name` is
+ *               the name of the test
+ * @param scheduler      The scheduler used to schedule the health tests
+ * @param metrics        The metrics to be updated
+ * @param pagerDutyAlert A function which will alert pager duty
+ * @param runnerProps    The props of the tests to be run
+ */
 class HealthCheck(name:             String,
                   config:           Config,
                   scheduler:        Scheduler,
@@ -203,14 +203,14 @@ class HealthCheck(name:             String,
 object HealthCheck {
 
   def props(
-             name:             String,
-             config:           Config,
-             scheduler:        Scheduler,
-             metrics:          Metrics,
-             pagerDutyAlert:   () ⇒ Unit,
-             pagerDutyResolve: () ⇒ Unit,
-             runnerProps:      Props,
-             otherRunnerProps: Props*): Props =
+      name:             String,
+      config:           Config,
+      scheduler:        Scheduler,
+      metrics:          Metrics,
+      pagerDutyAlert:   () ⇒ Unit,
+      pagerDutyResolve: () ⇒ Unit,
+      runnerProps:      Props,
+      otherRunnerProps: Props*): Props =
     Props(
       new HealthCheck(
         name, config, scheduler, metrics, pagerDutyAlert, pagerDutyResolve, NonEmptyList(runnerProps, otherRunnerProps.toList))
@@ -219,22 +219,22 @@ object HealthCheck {
   case object PerformTest
 
   /**
-    * Uses the `NonEmptyVector` to produce a queue which when dequeueing an element will
-    * put the dequeued element back on the end of the queue. In this way, this queue is
-    * never-ending because once the elements in the `NonEmptyVector` have been exhausted
-    * the queue will dequeue the same elements again. For example:
-    * {{{
-    *   val queue = NonEmptyCyclicalQueue(NonEmptyVector("a", Vector("b", "c"))
-    *   queue.next()  // = "a"
-    *   queue.next()  // = "b"
-    *   queue.next()  // = "c"
-    *   queue.next()  // = "a"
-    *   queue.next()  // = "b"
-    *   queue.next()  // = "c"
-    *   queue.next()  // = "a"
-    *   ...
-    * }}}
-    */
+   * Uses the `NonEmptyVector` to produce a queue which when dequeueing an element will
+   * put the dequeued element back on the end of the queue. In this way, this queue is
+   * never-ending because once the elements in the `NonEmptyVector` have been exhausted
+   * the queue will dequeue the same elements again. For example:
+   * {{{
+   *   val queue = NonEmptyCyclicalQueue(NonEmptyVector("a", Vector("b", "c"))
+   *   queue.next()  // = "a"
+   *   queue.next()  // = "b"
+   *   queue.next()  // = "c"
+   *   queue.next()  // = "a"
+   *   queue.next()  // = "b"
+   *   queue.next()  // = "c"
+   *   queue.next()  // = "a"
+   *   ...
+   * }}}
+   */
   private[health] case class NonEmptyCyclicalQueue[A](xs: NonEmptyList[A]) {
 
     private var queue: List[A] = xs.toList
