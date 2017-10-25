fdf53aa1eb6cefadb5b55845df09973b342f83ad
Andy <andrew.chung@digital.hmrc.gov.uk>
2017-10-25 10:23:40 +0100

    updated whitelist fitler

--- app/uk/gov/hmrc/helptosavefrontend/config/WhitelistFilter.scala
+++ app/uk/gov/hmrc/helptosavefrontend/config/WhitelistFilter.scala
@@ -19,17 +19,29 @@ package uk.gov.hmrc.helptosavefrontend.config
 import akka.stream.Materializer
 import configs.syntax._
 import play.api.Configuration
-import play.api.mvc.Call
+import play.api.mvc.{Call, RequestHeader, Result, Results}
+import uk.gov.hmrc.helptosavefrontend.util.Logging
 import uk.gov.hmrc.whitelist.AkamaiWhitelistFilter
 
+import scala.concurrent.Future
 
-class WhitelistFilter(configuration: Configuration, materializer: Materializer) extends AkamaiWhitelistFilter {
+class WhitelistFilter(configuration: Configuration, materializer: Materializer) extends AkamaiWhitelistFilter with Logging {
 
   override def whitelist: Seq[String] =
     configuration.underlying.get[List[String]]("http-header-ip-whitelist").value
 
-  override def destination: Call = Call("GET", "not-allowed")
+
+  // This is the `Call` used in the `Redirect` when an IP is present in the header
+  // of the HTTP request but is not in the whitelist
+  override def destination: Call = Call("GET", "http://not-allowed")
 
   override implicit def mat: Materializer = materializer
+
+  // if there is no IP address in the header return a `Forbidden` status
+  override def noHeaderAction(f:  (RequestHeader) ⇒ Future[Result],
+                              rh: RequestHeader): Future[Result] = {
+    logger.info("Whitelisting enabled but no IP found in header")
+    Future.successful(Results.Forbidden)
+  }
 
 }
