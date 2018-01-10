32db651812b1a65fa3a824c1c64ce48d48d76edd
Andy <andrew.chung@digital.hmrc.gov.uk>
2018-01-10 11:58:38 +0000

    some refactoring

--- run_selenium_system_test_local_chrome_service_manager.sh
+++ run_selenium_system_test_local_chrome_service_manager.sh
@@ -1,18 +1,25 @@
 #! /bin/sh
 
+exit_if_status_1() {
+  if [ $? = 1 ]
+  then
+     exit
+  fi
+}
+
 echo "Starting Service Manager with HTS_ALL"
 ./start-service-manager.sh
-if [ $? = 1 ]
-then
-    exit
-fi
+exit_if_status_1
+
+./services-health-check.sh
+exit_if_status_1
 
 echo "./run_selenium_system_test.sh local chrome /usr/local/bin/chromedriver"
 ./run_selenium_system_test.sh local chrome /usr/local/bin/chromedriver
 EXIT_CODE=$?
 echo "EXIT_CODE = $EXIT_CODE"
 
-./services-health-check.sh
+
 ./output-log-exceptions.sh
 
 sm --stop HTS_ALL
