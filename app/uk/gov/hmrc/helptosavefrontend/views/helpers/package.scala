c3c23bd61be69f9a13c8ed9033635b1cc475dc96
Andy <andrew.chung@digital.hmrc.gov.uk>
2018-07-18 11:37:40 +0100

    HTS-1115 - fixed newlines in page titles

--- app/uk/gov/hmrc/helptosavefrontend/views/helpers/package.scala
+++ app/uk/gov/hmrc/helptosavefrontend/views/helpers/package.scala
@@ -1,4 +1,4 @@
-@*
+/*
  * Copyright 2018 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,9 +12,20 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *@
+ */
 
-@(form: Form[_])(implicit messages: Messages)
-@if(form.hasErrors){
-  @Messages("hts.global.error.prefix")
+package uk.gov.hmrc.helptosavefrontend.views
+
+import play.api.data.Form
+import play.api.i18n.Messages
+
+package object helpers {
+
+  def errorPrefix(form: Form[_])(implicit messages: Messages): String =
+    if (form.hasErrors) {
+      Messages("hts.global.error.prefix")
+    } else {
+      ""
+    }
+
 }
