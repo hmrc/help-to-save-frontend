58abf45ed35f3e466206e35867c44c1a167037f4
Jacqueline Hubbard <Jackie1988@users.noreply.github.com>
2018-08-28 16:09:32 +0100

    HTS-1252 Added account created page showing account number (#630)

--- test/uk/gov/hmrc/helptosavefrontend/connectors/HelpToSaveConnectorSpec.scala
+++ test/uk/gov/hmrc/helptosavefrontend/connectors/HelpToSaveConnectorSpec.scala
@@ -29,7 +29,7 @@ import uk.gov.hmrc.helptosavefrontend.TestSupport
 import uk.gov.hmrc.helptosavefrontend.connectors.HelpToSaveConnectorImpl.GetEmailResponse
 import uk.gov.hmrc.helptosavefrontend.models.TestData.UserData.validNSIUserInfo
 import uk.gov.hmrc.helptosavefrontend.models._
-import uk.gov.hmrc.helptosavefrontend.models.account.{Account, Blocking}
+import uk.gov.hmrc.helptosavefrontend.models.account.{Account, AccountNumber, Blocking}
 import uk.gov.hmrc.helptosavefrontend.models.eligibility.EligibilityCheckResponse
 import uk.gov.hmrc.helptosavefrontend.models.eligibility.EligibilityCheckResult.{AlreadyHasAccount, Eligible, Ineligible}
 import uk.gov.hmrc.helptosavefrontend.models.register.CreateAccountRequest
@@ -342,7 +342,7 @@ class HelpToSaveConnectorSpec extends TestSupport with HttpSupport with Generato
 
       "return http response as it is to the caller" in {
         val request = CreateAccountRequest(validNSIUserInfo, 7)
-        val response = HttpResponse(201, Some(Json.toJson(SubmissionSuccess(false))))
+        val response = HttpResponse(201, Some(Json.toJson(SubmissionSuccess(Some(AccountNumber("1234567890123"))))))
         mockPost(createAccountURL, Map.empty[String, String], request)(Some(response))
         await(connector.createAccount(request)) shouldBe response
       }
