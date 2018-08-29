321d3e801ee4a833521d478ee5f10e142c3fae98
Alistair Macdonald <ali@nubz.com>
2018-08-29 16:17:02 +0100

    HTS-1245: check your details (#636)

--- test/uk/gov/hmrc/helptosavefrontend/controllers/RegisterControllerSpec.scala
+++ test/uk/gov/hmrc/helptosavefrontend/controllers/RegisterControllerSpec.scala
@@ -340,7 +340,7 @@ class RegisterControllerSpec
         val result = doRequest()
 
         status(result) shouldBe 200
-        contentAsString(result) should include("check your details")
+        contentAsString(result) should include("Check your details")
       }
 
       "show user not eligible page if the user is not eligible" in {
