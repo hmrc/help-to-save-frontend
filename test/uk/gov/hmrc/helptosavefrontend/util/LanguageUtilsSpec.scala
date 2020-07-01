c4c6257dcbfc4ee8bbbd6d9a743a35fca56b842d
amarnathgujjalapudi <amarnath.gujjalapudi@digital.hmrc.gov.uk>
2020-07-01 17:07:18 +0100

    DLS-2383 Welsh messages updated

--- test/uk/gov/hmrc/helptosavefrontend/util/LanguageUtilsSpec.scala
+++ test/uk/gov/hmrc/helptosavefrontend/util/LanguageUtilsSpec.scala
@@ -1,6 +1,17 @@
 /*
  * Copyright 2020 HM Revenue & Customs
  *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package uk.gov.hmrc.helptosavefrontend.util
@@ -16,20 +27,20 @@ class LanguageUtilsSpec extends UnitSpec {
       val date: LocalDate = LocalDate.of(2020, 6, 10)
       val easyReadingDate: String = LanguageUtils.formatLocalDate(date)(Lang("en"))
 
-      assert( easyReadingDate == "Wednesday 10 June 2020" )
+      assert(easyReadingDate == "Wednesday 10 June 2020")
     }
 
     "return a easy reading Welsh date " in {
       val date: LocalDate = LocalDate.of(2020, 6, 10)
       val easyReadingDate: String = LanguageUtils.formatLocalDate(date)(Lang("cy"))
-      assert( easyReadingDate == "Dydd Mercher 10 Mehefin 2020")
+      assert(easyReadingDate == "Dydd Mercher 10 Mehefin 2020")
     }
 
     "return a 12 hours timestamp  " in {
       val time: LocalTime = LocalTime.of(14, 16)
       val easyReadingTimestamp: String = LanguageUtils.formatLocalTime(time)
 
-      assert( easyReadingTimestamp == "2:16pm" )
+      assert(easyReadingTimestamp == "2:16pm")
     }
 
   }
