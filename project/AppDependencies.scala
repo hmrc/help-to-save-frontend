2f94616b50685361b24cafbd397c92910eeca81d
JakeReid2020 <75027964+JakeReid2020@users.noreply.github.com>
2021-11-01 15:41:46 +0000

    [DLS-4423][JR]: play 2.8 Update

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -29,7 +29,8 @@ object AppDependencies {
     "com.typesafe.play"      %% "play"                        % "2.8.8"             % "test",
     "com.miguno.akka"        %% "akka-mock-scheduler"         % "0.5.5"             % "test",
     "uk.gov.hmrc"            %% "bootstrap-test-play-28"      % "5.16.0"            % "test",
-    "com.vladsch.flexmark"    %  "flexmark-all"                 % "0.35.10"           % "test"
+    "com.vladsch.flexmark"    %  "flexmark-all"                 % "0.35.10"           % "test",
+
   )
 
   def apply(): Seq[ModuleID] = compile ++ test
