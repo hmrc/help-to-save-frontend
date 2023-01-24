dfdb41f87ca04c4a1b50dd735e67ddca2f4c584a
Alistair Macdonald <1460971+nubz@users.noreply.github.com>
2023-01-24 14:13:23 +0000

    new summary helper

--- app/uk/gov/hmrc/helptosavefrontend/views/helpers/FormErrorMessage.scala
+++ app/uk/gov/hmrc/helptosavefrontend/views/helpers/FormErrorMessage.scala
@@ -18,23 +18,26 @@ package uk.gov.hmrc.helptosavefrontend.views.helpers
 
 import play.api.data.Form
 import play.api.i18n.Messages
+import play.twirl.api._
 import uk.gov.hmrc.govukfrontend.views.Aliases.Text
 import uk.gov.hmrc.govukfrontend.views.viewmodels.errormessage.ErrorMessage
+import uk.gov.hmrc.govukfrontend.views.html.components._
+import uk.gov.hmrc.helptosavefrontend.views.ViewHelpers
+import javax.inject.Inject
 
-object FormErrorMessage {
+class FormErrorMessage @Inject()(ui: ViewHelpers) {
 
   def errorSummary(formName: String, form: Form[_])(
-      implicit messages: Messages): Option[GovukErrorSummary] = {
+      implicit messages: Messages): Option[HtmlFormat.Appendable] =
     if(form.errors.nonEmpty) {
-      @ui.govukErrorSummary(ErrorSummary(errorList = form.errors.map(e =>
+      Some(ui.govukErrorSummary(ErrorSummary(errorList = form.errors.map(e =>
         ErrorLink(
           href = Some(s"#${e.key}"),
           content = Text(s"${messages(s"${formName}.${e.key}.${e.message}")}")
         )
       ),
-        title = Text(messages("hts.global.error-summary.title"))))
+        title = Text(messages("hts.global.error-summary.title")))))
     } else None
-  }
   def formErrorMessage(formName: String, form: Form[_], key: String)(
     implicit messages: Messages): Option[ErrorMessage] =
     form
