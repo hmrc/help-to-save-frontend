e448f7fccf97f402d41132ba7dd1408f1f15b1f7
Alistair Macdonald <1460971+nubz@users.noreply.github.com>
2023-02-09 16:40:01 +0000

    [DLS-6869] add new email error handler and upgrade give_email

--- app/uk/gov/hmrc/helptosavefrontend/forms/EmailValidation.scala
+++ app/uk/gov/hmrc/helptosavefrontend/forms/EmailValidation.scala
@@ -28,7 +28,7 @@ import play.api.data.Forms.text
 import play.api.data.format.Formatter
 import play.api.data.{Form, FormError}
 import uk.gov.hmrc.helptosavefrontend.forms.EmailValidation._
-import uk.gov.hmrc.helptosavefrontend.util.Validation._
+import uk.gov.hmrc.helptosavefrontend.util.Validation.{ValidOrErrorStrings, _}
 
 import scala.annotation.tailrec
 
@@ -118,8 +118,10 @@ class EmailValidation @Inject() (configuration: Configuration) {
 
       val validation: Validated[NonEmptyList[String], String] =
         data.get(key).fold(invalid[String](ErrorMessages.blankEmailAddress))(validate)
-
-      validation.toEither.leftMap(_.map(e ⇒ FormError(key, e)).toList)
+      // previously this was returning a list of multiple errors on the same field
+      // now the hierarchy of which single error to return is determined by the order
+      // of the list returned by validate method
+      validation.toEither.leftMap(_.map(e ⇒ List(FormError(key, e))).head)
     }
 
     override def unbind(key: String, value: String): Map[String, String] =
