c039e3d7d7b9bd4b275e79137887592603894e0e
Alistair Macdonald <1460971+nubz@users.noreply.github.com>
2024-02-12 16:52:13 +0000

    [DLS-9901] Progress messaging

--- app/assets/javascripts/prevent-multiple-submission.js
+++ app/assets/javascripts/prevent-multiple-submission.js
@@ -1,11 +1,24 @@
-(function() {
+(function(window, document) {
     for (const form of document.getElementsByTagName("form")) {
       if (form.dataset.preventMultipleSubmit === "true") {
         form.addEventListener("submit", () => {
           for (const submit of form.querySelectorAll("button")) {
             submit.disabled = true;
           }
-        });
+          const progressIndicator = document.getElementById('submission-progress')
+          const liveRegionContent = document.getElementById('live-region-content')
+          if(progressIndicator && liveRegionContent) {
+            progressIndicator.setAttribute("tabindex", "-1")
+            progressIndicator.focus()
+            window.setTimeout(function(){
+              progressIndicator.removeAttribute("tabindex")
+              progressIndicator.classList.add("active")
+            }, 10)
+            window.setTimeout(function(){
+              progressIndicator.appendChild(liveRegionContent.content)
+            }, 20)
+          }
+        })
       }
     }
-})();
+})(window, document);
