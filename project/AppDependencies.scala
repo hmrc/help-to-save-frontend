f9d5fa3a016ef2cb24ec4b24fd6df5eeac029fc9
ruthdas03 <86231805+ruthdas03@users.noreply.github.com>
2025-09-12 18:29:46 +0100

    [DLS-8622] pr bot fixes

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,16 +4,16 @@ object AppDependencies {
 
   val hmrc = "uk.gov.hmrc"
   val playVersion = "play-30"
-  val bootstrapBackendVersion = "9.16.0"
+  val bootstrapBackendVersion = "9.19.0"
   val mockitoScalaVersion = "1.17.37"
-  val mongoVersion = "2.6.0"
+  val mongoVersion = "2.7.0"
 
   val compile: Seq[ModuleID] = Seq(
     s"$hmrc.mongo"      %% s"hmrc-mongo-$playVersion"         % mongoVersion,
     hmrc                %% s"bootstrap-frontend-$playVersion" % bootstrapBackendVersion,
     "org.typelevel"     %% "cats-core"                        % "2.13.0",
-    hmrc                %% s"domain-$playVersion"                           % s"11.0.0",
-    hmrc                %% s"play-frontend-hmrc-$playVersion"               % "11.13.0"
+    hmrc                %% s"domain-$playVersion"                           % s"13.0.0",
+    hmrc                %% s"play-frontend-hmrc-$playVersion"               % "12.12.0"
   )
 
   def test(scope: String = "test"): Seq[ModuleID] = Seq(
